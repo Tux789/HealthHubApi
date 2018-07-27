61c10dc91415ec9143536d0efcd04506533ceb4b
Jeff Swafford <jlswafford789@yahoo.com>
2018-07-26 21:32:44 -0500

    Take 3

--- routes/index.js
+++ routes/index.js
@@ -16,21 +16,21 @@ router.post("/api/signup",
           return res.status("500").send({success: false, message: "ERROR: " + err}); // will generate a 500 error
         }
         // Generate a JSON response reflecting authentication status
-        if (! user) {
-          return res.status("401").send({ success : false, message : 'authentication failed' });
-        }
+        // if (! user) {
+        //   return res.status("401").send({ success : false, message : 'authentication failed' });
+        // }
         // ***********************************************************************
         // "Note that when using a custom callback, it becomes the application's
         // responsibility to establish a session (by calling req.login()) and send
         // a response."
         // Source: http://passportjs.org/docs
         // ***********************************************************************
-        req.login(user, loginErr => {
-          if (loginErr) {
-            return next(loginErr);
-          }
-          return res.send({ success : true, message : 'authentication succeeded' });
-        });      
+        // req.login(user, loginErr => {
+        //   if (loginErr) {
+        //     return next(loginErr);
+        //   }
+        //   return res.send({ success : true, message : 'authentication succeeded' });
+        // });      
       })(req, res, next);
 });
 router.get("/api/autherror", (
