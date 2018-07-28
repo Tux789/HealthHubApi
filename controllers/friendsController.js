8f0cf78af10b419d55833b6b1673e2566752767f
Jeff Swafford <jlswafford789@yahoo.com>
2018-07-28 12:50:45 -0500

    now with proper exports

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 
+exports.default = {
 getFriends = (req, res) =>{
     console.log(req.user);
    db.User.findById(req.user.id)
@@ -7,11 +8,12 @@ getFriends = (req, res) =>{
        res.json(dbUser.friends);
     })
     .catch((err) => res.status("500").send(err));
-}
+},
 addFriends = (req, res) =>{
     db.User.findOneAndUpdate({_id: req.user.id}, {$push: {friends: req.params.friendId}},{new: true, upsert:true})
     .then((results)=>{
         console.log(results);
         res.json(results);
     })
+},
 }
